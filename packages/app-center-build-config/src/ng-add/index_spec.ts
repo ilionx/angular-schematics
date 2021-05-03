6c17830eef99b55e334d9142d76fb9957899e374
Kevin Boosten <kboosten@ilionx.com>
2021-05-03 14:50:28 +0200

    test: run unit and sandbox tests on git push

--- packages/app-center-build-config/src/ng-add/index_spec.ts
+++ packages/app-center-build-config/src/ng-add/index_spec.ts
@@ -1,14 +1,28 @@
 import { Tree } from '@angular-devkit/schematics';
 import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
 import * as path from 'path';
+import { Schema } from './schema';
 
 const collectionPath = path.join(__dirname, '../collection.json');
 
 describe('app-center-build-config', () => {
   it('works', async () => {
     const runner = new SchematicTestRunner('schematics', collectionPath);
-    const tree = await runner.runSchematicAsync('ng-add', {}, Tree.empty()).toPromise();
+    const tree = await runner
+      .runSchematicAsync<Schema>(
+        'ng-add',
+        {
+          appId: 'com.ilionx.dummy',
+        },
+        Tree.empty()
+      )
+      .toPromise();
 
-    expect(tree.files).toEqual([]);
+    expect(tree.files).toEqual([
+      '/DEPLOYMENT.md',
+      '/appcenter-post-clone-root.sh',
+      '/ios/App/appcenter-post-clone.sh',
+      '/android/app/appcenter-post-clone.sh',
+    ]);
   });
 });
